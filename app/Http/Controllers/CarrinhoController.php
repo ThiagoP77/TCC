078af3db42c4c7b4482af60839fcd0d9f178dfcb
Thiago Piffer Lauro <120656984+ThiagoP77@users.noreply.github.com>
2024-10-15 11:34:58 -0300

    Mudando mensagem de erro

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -681,7 +681,7 @@ class CarrinhoController extends Controller
         } catch (Exception $e) {//Captura exceção e envia mensagem de erro
 
                 return response()->json([
-                    'mensagem' => 'Erro ao excluir do carrinho.',
+                    'mensagem' => 'Erro ao listar carrinho.',
                     'erro' => $e->getMessage()
                 ], 400);
 
